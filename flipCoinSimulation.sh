5cf97d16cc0287fed89f84cb97fa18dc62144437
Jayesh7744 <Jayeshs7744@gmail.com>
2022-05-28 16:42:30 +0530

    Added: Multiple Simulation

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -8,3 +8,29 @@ then
 else
 	echo "Tail"
 fi
+
+
+Head=1
+Headcount=0
+Tailcount=0
+for((i=1 ; i<=21 ; i++))
+do
+  if((randomcheck=$((RANDOM%2))))
+  then
+	randomcheck=$((randomcheck==1))
+	Headcount=$(($Headcount + 1))
+        echo "Head"
+  else
+        Tailcount=$(($Tailcount + 1))
+	echo "Tail"
+  fi
+done
+echo "Headcount:" $Headcount
+echo "Tailcount:" $Tailcount
+
+  if(($Headcount>$Tailcount))
+  then
+	echo "Head_won:" $Headcount
+  else
+	echo "Tail_won:" $Tailcount
+  fi
