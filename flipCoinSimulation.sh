e3d127052f6fbaed42c628aead815d6737837988
Jayesh7744 <Jayeshs7744@gmail.com>
2022-05-28 16:55:47 +0530

    Added: Modify UC2

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -34,3 +34,34 @@ echo "Tailcount:" $Tailcount
   else
 	echo "Tail_won:" $Tailcount
   fi
+
+Head=1
+Tail=0
+Hcount=0
+Tcount=0
+
+while (( Head != Tail ))
+do
+if(( $((RANDOM % 2)) == 1 ))
+then
+        Hcount=$(($Hcount + 1))
+        echo "HEADS: $Hcount"
+
+        if(($Hcount == 21 ))
+        then
+                echo "HEAD WINS"
+                break
+        fi
+else
+        Tcount=$(($Tcount + 1))
+        echo "TAILS: $Tcount"
+
+        if [ $Tcount == 21 ]
+        then
+                echo "TAIL WINS"
+                break
+        fi
+fi
+done
+echo "Hcount:" $Hcount
+echo "Tcount:" $Tcount
